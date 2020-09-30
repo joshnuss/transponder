991c90906543a036b0625f5a8f59babe13ecf772
Josh Nussbaum <joshnuss@gmail.com>
2020-09-29 20:00:38 -0400

    Adds defaction macro

--- lib/resource_controller.ex
+++ lib/resource_controller.ex
@@ -2,4 +2,22 @@ defmodule ResourceController do
   @moduledoc """
   Documentation for ResourceController.
   """
+
+  defmacro __using__(opts) do
+    responder = Keyword.fetch!(opts, :responder)
+
+    quote do
+      import ResourceController
+
+      @responder unquote(responder)
+    end
+  end
+
+  defmacro defaction(name, fun) do
+    quote do
+      def unquote(name)(conn, params) do
+        @responder.respond(unquote(name), conn, unquote(fun))
+      end
+    end
+  end
 end
--- test/resource_controller_test.exs
+++ test/resource_controller_test.exs
@@ -1,4 +1,44 @@
 defmodule ResourceControllerTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+  use Plug.Test
   doctest ResourceController
+
+  defmodule TestResponder do
+    import Plug.Conn
+
+    @behaviour ResourceController.Responder
+
+    @impl true
+    def respond(:special, conn, _fun) do
+      resp(conn, 200, "special-case")
+    end
+    def respond(action, conn, fun) do
+      response = "#{action}:#{fun.()}"
+      resp(conn, 200, response)
+    end
+  end
+
+  defmodule FakeController do
+    use ResourceController, responder: TestResponder
+
+    defaction :index, fn -> 123 end
+    defaction :create, fn -> 456 end
+    defaction :special, fn -> 123 end
+  end
+
+  test "renders response" do
+    conn = conn(:get, "/foo")
+
+    conn = FakeController.index(conn, %{})
+    assert conn.status == 200
+    assert conn.resp_body == "index:123"
+
+    conn = FakeController.create(conn, %{})
+    assert conn.status == 200
+    assert conn.resp_body == "create:456"
+
+    conn = FakeController.special(conn, %{})
+    assert conn.status == 200
+    assert conn.resp_body == "special-case"
+  end
 end

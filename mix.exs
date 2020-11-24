010ada6e0a54eb1466bf0e26ae81595be481ca64
Josh Nussbaum <joshnuss@gmail.com>
2020-11-23 21:22:27 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Transponder.MixProject do
   def project do
     [
       app: :transponder,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       description: "DSL for declarative Phoenix controllers",
